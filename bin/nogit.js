a195fecfb1f9c2bf28d5f558364fc998e44f2175
luii <philipp.czarnetzki@protonmail.ch>
2016-08-06 15:28:49 +0200

    bin: update nogit.js

--- bin/nogit.js
+++ bin/nogit.js
@@ -4,28 +4,14 @@
 
 const rc = require('rc')('nogit')
 const pkg = require('../package.json')
-const path = require('path')
 let cli = require('yargs')
 
-cli.commandDir('remote')
-   .commandDir('rev-list')
-   .commandDir('log')
-   .commandDir('clone')
+cli.commandDir('add')
    .commandDir('checkout')
+   .commandDir('clone')
    .commandDir('config')
+   .commandDir('log')
+   .commandDir('remote')
+   .commandDir('rev-list')
    .commandDir('status')
-   .commandDir('add')
    .help().argv
-
-// program
-//   .version(pkg.version)
-//   .command('remote', __('remote.description'))
-//   .command('status', __('status.description'), { isDefault: true })
-//   .command('clone <repository> [cwd]', __('clone.description'))
-//   .command('push', __('push.description'))
-//   .command('pull', __('pull.description'))
-//   .command('log [cwd]', __('log.description'))
-//   .command('add <file>', __('add.description'))
-//   .command('commit', __('commit.description'))
-//   .command('rev-list', __('rev-list.description'))
-//   .parse(process.argv)
