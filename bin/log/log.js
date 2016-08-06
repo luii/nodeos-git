fe53030f01913be7b89510af8fd22d10d5feca2d
luii <philipp.czarnetzki@protonmail.ch>
2016-08-06 14:57:22 +0200

    lib: wrap log command

--- bin/log/log.js
+++ bin/log/log.js
@@ -37,24 +37,6 @@ exports.builder = {}
  * @return {Void}
  */
 exports.handler = function (argv) {
-  let gitFolder = path.join(process.cwd(), '/.git')
 
-  Repository.open(gitFolder).then(repo => {
-    return repo.getMasterCommit()
-  }).then(masterCommit => {
-    let count = 0
-    let history = masterCommit.history()
-
-    history.on("commit", commit => {
-      if (++count >= 9) return
-      let author = commit.author()
-
-      console.log(`Commit:\t ${bold(commit.sha())}`)
-      console.log(`Author:\t ${author.name()} <${author.email()}>`)
-      console.log(`Date:\t ${commit.date()}`)
-      console.log(`\n\t ${chalk.yellow(commit.message())}`)
-    })
-
-    history.start()
-  }).catch(console.log.bind(console))
+  nogit.log().catch(console.log.bind(console))
 }
