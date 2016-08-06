54498253e6d0f6f6850d9b04d682fffffe6126b7
luii <philipp.czarnetzki@protonmail.ch>
2016-08-06 15:08:55 +0200

    lib: wrap rev-list command

--- bin/rev-list/rev-list.js
+++ bin/rev-list/rev-list.js
@@ -3,11 +3,7 @@
 'use strict'
 
 // Dependencies
-const git = require('nodegit')
-const path = require('path')
-
-const Revwalk = git.Revwalk
-const Repository = git.Repository
+const nogit = require('../../lib')
 
 
 /**
@@ -37,30 +33,6 @@ exports.builder = {
 
 
 exports.handler = function (argv) {
-  let gitFolder = path.join(process.cwd(), '/.git')
-  let headCommit = null
-  let repo = null
-  let count = 0
-
-  Repository.open(gitFolder).then(repository => {
-    repo = repository
-
-    if (argv.commit === 'master') {
-      return repository.getMasterCommit()
-    } else if (/Head/i.test(argv.commit)) {
-      return repository.getHeadCommit()
-    }
-  }).then(commit => {
-    headCommit = commit
-
-    return Revwalk.create(repo)
-  }).then(rev => {
-    rev.walk(headCommit.id(), (err, commitRev) => {
-      if (count >= argv.maxCount) return
-      if (commitRev) console.log(commitRev.id().tostrS());
-
-      count++
-      return
-    })
-  }).catch(console.log.bind(console))
+  nogit.revList(argv.commit, argv)
+    .catch(console.log.bind(console))
 }
