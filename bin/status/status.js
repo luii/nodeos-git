4d0f18ece7989ab09b020cd52ade8856c19927b7
luii <philipp.czarnetzki@protonmail.ch>
2016-08-06 15:26:06 +0200

    lib: wrap status command

--- bin/status/status.js
+++ bin/status/status.js
@@ -3,22 +3,14 @@
 'use strict'
 
 // Dependencies
-const git = require('nodegit')
-const chalk = require('chalk')
-
-const Revwalk = git.Revwalk
-const Repository = git.Repository
-const RED_BOLD = chalk.red.bold
-const GREY_BOLD = chalk.grey.bold
-const GREEN_BOLD = chalk.green.bold
-const YELLOW_BOLD = chalk.yellow.bold
+const nogit = require('../../lib')
 
 /**
  * The command format itself
  * @access public
  * @type   {String}
  */
-exports.command = 'status <path>'
+exports.command = 'status [path]'
 
 /**
  * Description of the `remote` command
@@ -36,27 +28,6 @@ exports.builder = {}
 
 
 exports.handler = (argv) => {
-  let cwd = argv.path || process.cwd()
-  let gitFolder = path.join(cwd, '/.git')
-  let repo
 
-  Repository.open(gitFolder).then(repository => {
-    repo = repository
-    return repo.getStatus()
-  }).then(statuses => {
-    function statusToText(status) {
-      var words = []
-      if (status.isNew()) { words.push(GREEN_BOLD("NEW")) }
-      if (status.isModified()) { words.push(YELLOW_BOLD("MODIFIED")) }
-      if (status.isTypechange()) { words.push(YELLOW_BOLD("TYPECHANGE")) }
-      if (status.isRenamed()) { words.push(RED_BOLD("RENAMED")) }
-      if (status.isIgnored()) { words.push(GREY_BOLD("IGNORED")) }
-
-      return words.join(" ")
-    }
-
-    statuses.forEach(function(file) {
-      console.log(`${statusToText(file)} ${file.path()}`)
-    })
-  })
+  nogit.status(argv.path, argv).catch(console.log.bind(console))
 }
